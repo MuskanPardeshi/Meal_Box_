35603b632fdca48c218a70b6b643073642f98ce8
MuskanPardeshi <muskanpardeshi288@gmail.com>
2024-07-03 21:33:11 +0530

    Added custome hook to show Restaurant Menu

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,29 +1,20 @@
-import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
-import { MENU_URL } from "../utils/constants";
+
 import {useParams} from "react-router-dom"
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () =>{
-    const [resInfo, setResInfo] = useState(null)
     const {resId} = useParams()
-    console.log(resId)
+   const resInfo = useRestaurantMenu(resId)
 
-    const fetchMenu = async () => {
-        const data = await fetch(MENU_URL + resId)
-        const json = await data.json()
-        // console.log(json)
-        setResInfo(json.data)
-    }    
 
-    useEffect(()=>{
-        fetchMenu()
-    },[])
+ if(resInfo ===null) return <Shimmer />
 
-if(resInfo ===null) return <Shimmer />
+ const {name, cuisines, costForTwoMessage, avgRating} =
+ resInfo?.cards?.[2]?.card?.card?.info 
 
-const {name, cuisines, costForTwoMessage, avgRating} =  resInfo?.cards?.[2]?.card?.card?.info 
-
-const {itemCards} = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+ const {itemCards} =
+ resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
 
     return(
         <div className="menu">          
